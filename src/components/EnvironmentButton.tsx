34ec40c951bc24aa063fc26c7647430ad1fdc023
Felipe Souza <felipelsouza.dev@gmail.com>
2021-04-21 13:49:26 -0300

    Add Environments Button and Plant Card components

--- src/components/EnvironmentButton.tsx
+++ src/components/EnvironmentButton.tsx
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 12,
-    marginRight: 5,
+    marginHorizontal: 5,
   },
   containerActive: {
     color: colors.green,
