3de4f6b742cd2408e8ddbc25e6e671e537be6fb9
Felipe Souza <felipelsouza.dev@gmail.com>
2021-04-21 11:09:58 -0300

    Add Environment Button component

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import {
-  SafeAreaView,
   View,
   Text,
   StyleSheet,
@@ -16,15 +15,21 @@ import { Button } from '../components/Button';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
+import { EnvironmentButton } from '../components/EnvironmentButton';
 
 export function PlantSelect() {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <Text>Planta</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Header />
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>Você quer colocar sua planta?</Text>
+      </View>
+
+      <EnvironmentButton title="Cozinha" />
+    </View>
   );
 }
 
@@ -32,5 +37,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
   },
 });
