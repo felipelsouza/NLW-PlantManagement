aa95d882154ed2e8eab4fcf8026a3a3d9120e487
Felipe Souza <felipelsouza.dev@gmail.com>
2021-04-21 11:35:48 -0300

    Add api connection

--- src/components/EnvironmentButton.tsx
+++ src/components/EnvironmentButton.tsx
@@ -32,6 +32,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 12,
+    marginRight: 5,
   },
   containerActive: {
     color: colors.green,
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  Platform,
-  Keyboard,
-} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 import { Header } from '../components/Header';
-import { Button } from '../components/Button';
+
+import api from '../services/api';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 import { EnvironmentButton } from '../components/EnvironmentButton';
 
+interface EnvironmentProps {
+  key: string;
+  title: string;
+}
+
 export function PlantSelect() {
+  const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
+
   const navigation = useNavigation();
+
+  useEffect(() => {
+    async function fetchEnvironment() {
+      const { data } = await api.get('/plants_environments');
+      setEnvironments([{ key: 'all', title: 'Todos' }, ...data]);
+    }
+
+    fetchEnvironment();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -28,7 +36,17 @@ export function PlantSelect() {
         <Text style={styles.subtitle}>Você quer colocar sua planta?</Text>
       </View>
 
-      <EnvironmentButton title="Cozinha" />
+      <View>
+        <FlatList
+          data={environments}
+          renderItem={({ item }) => (
+            <EnvironmentButton title={item.title} key={item.key} />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.environmentList}
+        />
+      </View>
     </View>
   );
 }
@@ -53,5 +71,12 @@ const styles = StyleSheet.create({
     fontSize: 17,
     lineHeight: 20,
     color: colors.heading,
+  },
+  environmentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
   },
 });
