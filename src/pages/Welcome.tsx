32b80b65fc59df86189efa09dc91bb197d95f450
Felipe Souza <felipelsouza.dev@gmail.com>
2021-04-24 21:34:31 -0300

    Remove font weight

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -57,7 +57,6 @@ const styles = StyleSheet.create({
   title: {
     fontFamily: fonts.heading,
     fontSize: 28,
-    fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
     marginTop: 38,
